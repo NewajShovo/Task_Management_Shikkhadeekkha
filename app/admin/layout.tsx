f32e2dbde8eb6abffff7b6c3bff0e5cbbf89f979
NewajShovo <leoshovo@gmail.com>
2025-08-10 17:47:24 +0600

    Modification just to check CI/CD pipeline

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,9 +1,12 @@
-import type React from "react"
+import type React from "react";
+
 
 export default function AdminLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return <div className="min-h-screen bg-gray-50 dark:bg-gray-900">{children}</div>
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">{children}</div>
+  );
 }
